df24002a0dccb7621d3ca7473bd98c5f48b6cd32
Vegar Vikan <vegar@vi-kan.net>
2015-09-10 23:23:11 +0200

    ClickEmitter in front of sprite

--- src/objects/ClickEmitter.js
+++ src/objects/ClickEmitter.js
@@ -3,22 +3,22 @@ class ClickEmitter extends Phaser.Particles.Arcade.Emitter {
   constructor(game) {
   	super(game);
 
-    this.emitter = game.add.emitter(game.world.centerX, game.world.centerY);
+    //this.emitter = game.add.emitter(game.world.centerX, game.world.centerY);
 
-    this.emitter.makeParticles('containerParticle');
-    this.emitter.setAlpha(1, 0, 5000);
-    this.emitter.gravity = 200;
-    this.emitter.lifespan = 5000;
-    this.emitter.setYSpeed(-50, -200);
+    this.makeParticles('containerParticle');
+    this.setAlpha(1, 0, 5000);
+    this.gravity = 200;
+    this.lifespan = 5000;
+    this.setYSpeed(-50, -200);
 
 	this.game.stage.addChild(this);  
   }
 
   // methods
   emitParticle(x, y){
-  	this.emitter.emitX = x;
-  	this.emitter.emitY = y;
-  	this.emitter.emitParticle();
+  	this.emitX = x;
+  	this.emitY = y;
+  	super.emitParticle();
   }
 }
 
